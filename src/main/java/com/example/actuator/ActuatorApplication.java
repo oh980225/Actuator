9439a8ef2353f48181dc17767c26c40dd7e5c40b
oh980225 <oh980225@gmail.com>
2021-07-25 22:51:44 +0900

    [ADD] Spring Admin UI

--- src/main/java/com/example/actuator/ActuatorApplication.java
+++ src/main/java/com/example/actuator/ActuatorApplication.java
@@ -1,9 +1,11 @@
 package com.example.actuator;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class ActuatorApplication {
 
     public static void main(String[] args) {
